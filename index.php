b7e65a4f6611ca0301890a98dd1798b3df914ed0
Al Mikhtadov <lilwondermelo@gmail.com>
2020-09-09 23:18:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,20 @@
 <!DOCTYPE html>
 <html>
-
     <head>
         <title>Mommyfy</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/mommyfy/media/images/icons/coins.svg">
         <link rel="icon" type="image/png" sizes="32x32" href="/mommyfy/media/images/icons/coins.svg">
         <link rel="icon" type="image/png" sizes="16x16" href="/mommyfy/media/images/icons/coins.svg">
-        <link rel="manifest" href="/images/favicon/site.webmanifest">
-        <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="theme-color" content="#ffffff">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.2">
-        <meta name="format-detection" content="telephone=no">
-        <meta name="keywords" content="Printing Tecnologies">
-        <meta name="description" content="Printing Tecnologies">
-
         <link rel="stylesheet" type="text/css" href="style/style.css">
-        <link rel="stylesheet" type="text/css" href="style/style.css">
-        <link rel="stylesheet" type="text/css" href="style/style.css">
-
-        
+        <link rel="stylesheet" type="text/css" href="style/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="style/bootstrap-grid.css">
+        <script src="js/jquery-3.5.1.js"></script>
+        <script src="js/bootstrap.bundle.js"></script>
+		<script src="js/bootstrap.js"></script>
     </head>
     <body>
-   
-      	
-       
-       
-
-
-
+    	
     </body>
-
 </html>
