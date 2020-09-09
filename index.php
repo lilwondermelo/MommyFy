f70ee73ed0ac22cf89541690a30b09773267c1e7
Al Mikhtadov <lilwondermelo@gmail.com>
2020-09-09 23:15:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 
     <head>
         <title>Mommyfy</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/media/images/icons/coins.svg">
-        <link rel="icon" type="image/png" sizes="32x32" href="/media/images/icons/coins.svg">
-        <link rel="icon" type="image/png" sizes="16x16" href="/media/images/icons/coins.svg">
+        <link rel="apple-touch-icon" sizes="180x180" href="/mommyfy/media/images/icons/coins.svg">
+        <link rel="icon" type="image/png" sizes="32x32" href="/mommyfy/media/images/icons/coins.svg">
+        <link rel="icon" type="image/png" sizes="16x16" href="/mommyfy/media/images/icons/coins.svg">
         <link rel="manifest" href="/images/favicon/site.webmanifest">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#ffffff">
