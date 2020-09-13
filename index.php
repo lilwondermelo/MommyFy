979e5043cf74aa46574e6ede0eb7cbe5626ce18a
Al Mikhtadov <lilwondermelo@gmail.com>
2020-09-13 11:57:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
     <body>
         <div class="container">
         	<div class="row">
-        		<div class="col-md-6"></div>
-        		<div class="col-md-6"></div>
+        		<div class="col-md-6">123</div>
+        		<div class="col-md-6">123</div>
         	</div>
         </div>
     </body>
