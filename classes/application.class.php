9bf654da1d02c151cb717a8409e91aa6dd1d1283
Al Mikhtadov <lilwondermelo@gmail.com>
2020-09-14 12:54:46 +0700

    Update application.class.php

--- classes/application.class.php
+++ classes/application.class.php
@@ -6,7 +6,7 @@ class Application {
                 require_once $_SERVER['DOCUMENT_ROOT'] . '/mommyfy/core/_dataRowUpdater.class.php';
                 $updater = new DataRowUpdater('dir_users');
         	$updater->setKey('phone', "'" . $phone . "'");
-                $updater->setDataFields(array('`code`' => '1234'));
+                $updater->setDataFields(array('code' => '1234'));
                 $result = $updater->update();
                 if (!$result) {
                         $this->error = $updater->error;
